cd62ef26c43b6959649e0f7d2a08ac91d370aeb4
Akash Sharma <akash24497@gmail.com>
2024-08-11 23:24:29 +0530

    Commit new files

---
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/application/KitchenSinkApplication.java
@@ -0,0 +1,11 @@
+package org.jboss.as.quickstarts.kitchensink.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenSinkApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(KitchenSinkApplication.class, args);
+  }
+}
